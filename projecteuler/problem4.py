5fa75e5b6d65fd086d5475e78ecd8d8d887fe5f7
Home\Miguel <mpasa@deloitte.com.au>
2020-07-01 21:31:32 +1000

    updated problem4

--- projecteuler/problem4.py
+++ projecteuler/problem4.py
@@ -7,3 +7,17 @@
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 #########################################################################################################
+
+def isNumberAPalindrome(number):
+	lengthOfNumber = len(str(number))
+	for i in range (0, int(lengthOfNumber/2)+1):
+		if str(number)[i] != str(number)[-(i+1)]:
+			return bool(False)
+	return bool(True)
+
+print(isNumberAPalindrome(10222224201))
+
+for num in range (998001, 0, -1):
+	if isNumberAPalindrome(num):
+		print(num, " has been the largest so far")
+		exit()
